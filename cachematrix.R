63de47408d20f0ccbb0bcce7020c07502148f341
Srikanth M R <srikanth.mr@gmail.com>
2015-02-22 08:32:44 +0530

    22-Jan-2015: Added a global header and initiating my first commit for ProgramingAssignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,10 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This program allows users to compute mean of large vectors.
+## It uses lexical scoping in R to allow computation of mean over large vectors
+## Cached computing is critical for handling large data sets, especiaaly when
+## the data set doesnt fit into memory. 
+## This method can also be used for running computation of mean. This method 
+## will be useful for computing mean on large vectors that do not cahnge often.
+
 
 ## Write a short comment describing this function
 
